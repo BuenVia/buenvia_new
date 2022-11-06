d4c912dc5efe65c32580df62a46de0d866106336
SaracensFan <matthewclifford@hotmail.co.uk>
2022-11-06 18:48:54 +0000

    update

--- src/components/QuestionCard.jsx
+++ src/components/QuestionCard.jsx
@@ -2,22 +2,43 @@ import { useState } from 'react'
 import Button from 'react-bootstrap/Button'
 import Card from 'react-bootstrap/Card'
 import Form from 'react-bootstrap/Form'
+import vocab from '../vocab'
 
 export default function QuestionCard(props) {
 
+    const [content, setContent] = useState(vocab.colors)
+    const [index, setIndex] = useState(0)
+
+    console.log(content);
+
     function cancel() {
         return props.active()
+    }
+
+    function nextWord() {
+        if (index < content.length - 1) {
+            setIndex(prevVal => prevVal + 1)
+        } else {
+            cancel()
+        }
+    }
+
+    function playSound(text) {
+        text = content[index].esp
+        const speach = new SpeechSynthesisUtterance()
+        if (speechSynthesis.speaking) return
+        speach.lang = 'es-ES'
+        speach.text = text
+        speechSynthesis.speak(speach)
     }
 
     return (
         <div>
             <Card className='card--question'>
             <Button onClick={cancel} variant='danger'>Cancel</Button>
-            <Form>
-                <Form.Label>Spanish Word</Form.Label>
-                <Form.Control type="email" placeholder="Enter translation" />
-                <Button variant='success'>Submit</Button>
-            </Form>
+            <h1>{content[index].esp} <i onClick={playSound} className="fa-solid fa-volume-high"></i></h1>
+            <p>{content[index].eng}</p>
+            <Button onClick={nextWord}>Next</Button>
         </Card>
         </div>
     )
---
+++ src/vocab.js
@@ -0,0 +1,25 @@
+const vocab = {
+    colors: [
+        {eng: 'yellow', esp: 'amarillo'},
+        {eng: 'blue', esp: 'azul'},
+        {eng: 'white', esp: 'blanco'},
+        {eng: 'clear', esp: 'claro'},
+        {eng: 'colour', esp: 'el color'},
+    ],
+    common: [
+        {eng: 'open', esp: 'abierto'},
+        {eng: 'to finish', esp: 'acabar'},
+        {eng: 'fortunately', esp: 'por suerte'},
+        {eng: 'afuera', esp: 'outsdie'},
+        {eng: 'ahí', esp: 'there'},
+    ],
+    comparing: [
+        {eng: 'enough', esp: 'bastante'},
+        {eng: 'well', esp: 'bien'},
+        {eng: 'good', esp: 'bueno'},
+        {eng: 'like', esp: 'como'},
+        {eng: 'to compare', esp: 'comprarar'}
+    ]
+}
+
+export default vocab
