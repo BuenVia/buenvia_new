f4891afca2a861fc7ca2b346cb6674539868d2e5
SaracensFan <matthewclifford@hotmail.co.uk>
2022-11-06 06:51:14 +0000

    update

--- src/components/QuestionCard.jsx
+++ src/components/QuestionCard.jsx
@@ -1,12 +1,18 @@
+import { useState } from 'react'
 import Button from 'react-bootstrap/Button'
 
 export default function QuestionCard() {
 
+    const [isActive, setIsActive] = useState(false)
+
+    function handleClick() {
+        setIsActive(prevVal => !prevVal)
+    }
 
 
     return (
         <div>
-            <Button variant='primary'>Start</Button>
+            {isActive ? <Button onClick={handleClick} variant='danger'>Cancel</Button> : <Button onClick={handleClick} variant='primary'>Start</Button>}
         </div>
     )
 }
