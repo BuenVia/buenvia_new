1acfa211e6e65e515c6bd950f7a57fd4b6f89f46
SaracensFan <matthewclifford@hotmail.co.uk>
2022-11-12 12:58:33 +0000

    update

--- src/components/Learn.jsx
+++ src/components/Learn.jsx
@@ -1,10 +1,25 @@
+import { useEffect, useState } from 'react'
 import Button from 'react-bootstrap/Button'
 
 export default function Learn(props) {
+
+    const [vocabs, setVocabs] = useState([])
+
+    const url = 'http://localhost:9000/api/topics/count'
+
+    useEffect(() => {
+        fetch(url)
+        .then(response => response.json())
+        .then(json => setVocabs(json))
+    }, [])
+
     return(
         <div>
-           <p>Learn</p>
             <Button onClick={props.activity}>Home</Button>
+           <h3>Learn</h3>
+           {vocabs.map((vocab) => {
+            return (<div><Button>{vocab.name}</Button></div>)
+           })}
         </div>
     )
 }
